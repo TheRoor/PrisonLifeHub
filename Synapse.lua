2e47ab35eb4dfd63662ff0c9590f128a02f53886
TheRoor <122645633+TheRoor@users.noreply.github.com>
2023-02-19 17:27:48 +0000

    Update Synapse.lua

--- Synapse.lua
+++ Synapse.lua
@@ -18,38 +18,10 @@ Theme = 1
 }
 
 
+
+
 if game.PlaceId == 155615604 then
 
-   
-------------------Functions
-
-function  MODAK()
-Spawn(function ()
-     if game.Players.LocalPlayer.Backpack["AK-47"] then
-            
-        local AK = require(game.Players.LocalPlayer.Backpack["AK-47"].GunStates)
-        AK["AutoFire"] = true
-        AK["FireRate"] = 0.001
-        AK["Spread"] = 0
-        AK["Range"] = math.huge
-        AK["ReloadTime"] = 0.01
-
-        elseif  game.Players.LocalPlayer.Character["AK-47"] then
-            
-            local AKWO = require(game.Players.LocalPlayer.Character["AK-47"].GunStates)
-            AKWO["AutoFire"] = true
-            AKWO["FireRate"] = 0.0001
-            AKWO["Spread"] = 0
-            AKWO["Range"] = math.huge
-            AKWO["ReloadTime"] = 0.01
-         end 
-    end)
-end
-
-
-function UNMODAK()
-    
-end
 
 
     ----------SAVE
@@ -61,15 +33,18 @@ local function Save()
        
        local function Load()
         local Http = game:service("HttpService")
-        if isfile(File) then
+        
             _G.SettingS = Http:JSONDecode(readfile(File))
-            for i,v in pairs(_G.Settings) do
-                print(v)
-            end
-        end
+    
        end
 
-Load()
+       Load()
+print(_G.Settings.Theme)
+if _G.Settings.Theme == 3 then
+print("is3")
+print("is3")
+end
+
      ------------------GUNMOD
     local GM = Window:NewTab("GunMod")
 
@@ -82,7 +57,24 @@ Load()
 
         if state then
 
-          MODAK()
+            if game.Players.LocalPlayer.Backpack["AK-47"] then
+            
+                local AK = require(game.Players.LocalPlayer.Backpack["AK-47"].GunStates)
+                AK["AutoFire"] = true
+                AK["FireRate"] = 0.001
+                AK["Spread"] = 0
+                AK["Range"] = math.huge
+                AK["ReloadTime"] = 0.01
+        
+                elseif  game.Players.LocalPlayer.Character["AK-47"] then
+                    
+                    local AKWO = require(game.Players.LocalPlayer.Character["AK-47"].GunStates)
+                    AKWO["AutoFire"] = true
+                    AKWO["FireRate"] = 0.0001
+                    AKWO["Spread"] = 0
+                    AKWO["Range"] = math.huge
+                    AKWO["ReloadTime"] = 0.01
+                 end 
          
           else
             
@@ -205,7 +197,7 @@ Load()
 ---------------------COMBAT
 local CB = Window:NewTab("Combat")
 local CL = CB:NewSection("Combat Log")
-CL:NewLabel("Combat log TP's u when u low to crim base")
+CL:NewLabel("Combat log TP's u when u low")
 
 
 CL:NewSlider("HealthNeeded", "How much health needed before combat logging", 100, 0, function(s) 
@@ -213,39 +205,50 @@ CL:NewSlider("HealthNeeded", "How much health needed before combat logging", 100
      Save()
 end)
 
-CL:NewLabel(" RE-ENABLE IF USED")
+CL:NewLabel(" RE-ENABLE IF USED ALREADY")
 
 
 
 
 local CL = CL:NewToggle("CombatLog", "Tp's you safely when health matches slider", function(state)
-    Save()
-    if state then
-      
+
+    local thr = coroutine.create(function()
         local Teams = game:GetService("Teams")
         while task.wait() do
+           
             if cr.Humanoid.Health <= healthreq then
                 if lp.Team == Teams.Criminals then
                         local loc = CFrame.new(-941.97052, 94.1287613, 2058.86475, 0.0249914099, -9.90610687e-08, -0.999687672, 1.53281086e-08, 1, -9.87088313e-08, 0.999687672, -1.28564484e-08, 0.0249914099)
                         cr.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
                         cr.HumanoidRootPart.CFrame = loc
-                      
+                            break
                 elseif lp.Team == Teams.Guards then
                     local loc = CFrame.new(833.726746, 101.169716, 2296.53906, -0.990032434, -0.0351344757, 0.136386856, -1.72216208e-09, 0.968383968, 0.249464378, -0.140839651, 0.246977821, -0.958731532)
                     cr.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
                     cr.HumanoidRootPart.CFrame = loc
-                
+                    break
                 elseif lp.Team == Teams.Prisoners then
                     local loc = CFrame.new(-941.97052, 94.1287613, 2058.86475, 0.0249914099, -9.90610687e-08, -0.999687672, 1.53281086e-08, 1, -9.87088313e-08, 0.999687672, -1.28564484e-08, 0.0249914099)
                     cr.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
                     cr.HumanoidRootPart.CFrame = loc
-                   
-                end
-            end
-            task.wait(0.1)
-        end
-    else
+                        break
+                        end
+                 end
+           end  
+
+
+    end)
+
+    if state then
+
+       task.spawn(thr)
+        print(state)
+     
+        else
+        coroutine.yield(thr)
+        print(state)
     end
+   
 end)
 
 ---------------------TP
@@ -339,10 +342,6 @@ ThemePick:NewButton("DarkTheme", "Changes theme to DarkTheme", function()
     Save()
  end)
 
- ThemePick:NewButton("Midnight", "Changes theme to Midnight", function()
-    _G.Settings.Theme = 5
-    Save()
- end)
 
 local ThemeLoader = TH:NewSection("Load Themes")
 
@@ -351,10 +350,32 @@ ThemeLoader:NewLabel("Press button below to reload Script/Theme")
 ThemeLoader:NewButton("Reload Script for theme", "Reload's the script", function()
  if _G.Settings.Theme == 1 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRoor/PrisonLifeHub/main/Sentinal.lua"))()
- elseif _G.Settings.Theme == 2 then
+elseif _G.Settings.Theme == 2 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRoor/PrisonLifeHub/main/DarkTheme.lua"))()
+elseif _G.Settings.Theme == 3 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRoor/PrisonLifeHub/main/Synapse.lua"))()
-    end  
+elseif  _G.Settings.Theme == 4 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRoor/PrisonLifeHub/main/Ocean.lua"))()
+elseif _G.Settings.Theme == 5 then
+
+ end
+      
  end)
+
+local KB = Window:NewTab("Keybinds/Credits")
+
+local MN = KB:NewSection("Keybinds")
+
+MN:NewLabel("RightCtrl To Hide UI")
+
+MN:NewKeybind("Hide UI", "Hides the UI", Enum.KeyCode.RightControl, function()
+	Library:ToggleUI()
+end)
+
+local CR = KB:NewSection("Credits")
+
+CR:NewLabel("TheRoor#5735 - Scripts")
+CR:NewLabel("Kavo Ui Library - GUI")
 
 
 else --if no prison Life
@@ -362,3 +383,4 @@ else --if no prison Life
 local TP = game:GetService("TeleportService")
 lp:kick("No prison life")
 end -- end of if at top
+
